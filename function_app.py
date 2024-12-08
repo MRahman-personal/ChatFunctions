123efbfb3799063c122129e3a7a6a6034dd193cb
mahmu <mahmudur@umich.edu>
2024-12-08 17:57:42 -0500

    update connection

--- function_app.py
+++ function_app.py
@@ -21,7 +21,7 @@ container = database.create_container_if_not_exists(
 )
 
 @app.service_bus_queue_trigger(arg_name="azservicebus", queue_name="chatnotifications",
-                               connection="AzureWebJobsServiceBus") 
+                               connection="studentchatnotifications_SERVICEBUS") 
 def NotificationProcessor(azservicebus: func.ServiceBusMessage):
     try:
         message_body = azservicebus.get_body().decode('utf-8')
